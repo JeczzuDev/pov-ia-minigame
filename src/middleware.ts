0bb9248b2460050e2c085ce8bc4dee1fadd9d8a4
JeczzuDev <jesusignaciocc22@gmail.com>
2026-03-24 02:58:02 -0300

    feat(middleware): implement session update in clerk middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,19 +1,14 @@
 import { clerkMiddleware } from '@clerk/nextjs/server'
+import { updateSession } from '@/utils/supabase/middleware'
 
-// const isPublicRoute = createRouteMatcher(['/sign-in(.*)', '/sign-up(.*)', '/'])
-
-// export default clerkMiddleware(async (auth, req) => {
-//   if (!isPublicRoute(req)) {
-//     await auth.protect()
-//   }
-// })
-
-export default clerkMiddleware()
+export default clerkMiddleware(async (_auth, req) => {
+  return updateSession(req)
+})
 
 export const config = {
   matcher: [
     // Skip Next.js internals and all static files, unless found in search params
-    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    String.raw`/((?!_next|[^?]*\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)`,
     // Always run for API routes
     '/(api|trpc)(.*)',
   ],
