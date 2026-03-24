14d3f6bec61253c950e2b9dcaec5afefa9b95797
JeczzuDev <jesusignaciocc22@gmail.com>
2026-03-24 02:58:52 -0300

    refactor(supabase): extract environment variables for Supabase client initialization

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -1,8 +1,13 @@
 import { createClient } from '@supabase/supabase-js';
 
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
+const supabasePublicKey =
+  process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_DEFAULT_KEY ||
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
+
 const supabase = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+  supabaseUrl,
+  supabasePublicKey
 );
 
 export default supabase;
